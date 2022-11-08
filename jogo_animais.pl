f6b40145b7a6727f0ed0c38c781de0b57009d5b6
João Victor Effting <joaoefftings@gmail.com>
2022-11-08 19:18:45 -0300

    Update jogo_animais.pl

--- jogo_animais.pl
+++ jogo_animais.pl
@@ -14,3 +14,51 @@ carnivoro(X) :-
     dentes_pont(X),
     garras(X),
     olhos_frente(X).
+    
+puma(X) :-
+    mamifero(X),
+    manchas_negras(X),
+    cor_fulva(X),
+    carnivoro(X).
+
+tigre(X) :-
+    mamifero(X),
+    manchas_negras(X),
+    cor_fulva(X),
+    carnivoro(X).
+
+girafa(X) :-
+    ungulado(X),
+    pescoco_comprido(X),
+    perna_comprida(X),
+    manchas_negras(X).
+
+zebra(X) :-
+    ungulado(X),
+    listas_negras(X).
+
+avestruz(X) :-
+    ave(X),
+    nao_voa(X),
+    preto(X),
+    branco(X),
+    pescoco_comprido(X).
+
+pinguim(X) :-
+    ave(X),
+    nada(X),
+    nao_voa(X),
+    preto(X),
+    branco(X).
+
+albatros(X) :-
+    ave(X),
+    voa(X).
+
+morcego(X) :-
+    mamifero(X),
+    voa(X).
+
+baleia(X) :-
+    mamifero(X),
+    nao_tem_pelos(X).
